69b56cae474aba78317967299696319f14256928
Alexander Baker <38450939+acakebaker@users.noreply.github.com>
2021-12-31 01:34:28 -0600

    Update

--- udemy-season/src/components/SeasonDisplay.js
+++ udemy-season/src/components/SeasonDisplay.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+  if(month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+}
+
+const SeasonDisplay = ({ lat }) => {
+  const season = getSeason(lat, new Date().getMonth());
+  const text = season === 'winter' ? `Burr, it's chilly!` : `Lets hit the beach!`;
+
   return (
     <div>
-      Season Display
+      <h1>{text}</h1>
     </div>
-  )
-}
+  );
+};
 
 export default SeasonDisplay
--- udemy-season/src/index.js
+++ udemy-season/src/index.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import SeasonDisplay from './components/SeasonDisplay';
 
+// This is a class component app.
 class App extends React.Component {
-  render() {
-    // Gets the users location.
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (error) => console.log(error)
-    );
+  // This is the only time we do direct assignment to state!
+  state = { lat: null, errorMessage: '' };
+  
+  // componentDidMount() is for when the state starts.
+  componentDidMount() {
+    console.log('My component was rendered to the screen.');
 
-    return (
-      <div>Latitude: </div>
+    // This gets the users location.
+    window.navigator.geolocation.getCurrentPosition(
+      // To update our state we call setState!
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (error) => this.setState({ errorMessage: error.message })
     );
+  }
+
+  // componentDidUpdate() is for when the state updates.
+  componentDidUpdate() {
+    console.log('My component was just updated - it rerendered.');
+  }
+
+  // React says we have to define render()!
+  render() {
+    // Error message and no latitude.
+    if(this.state.errorMessage && !this.state.lat) {
+      return <div><h1>Error: {this.state.errorMessage}</h1></div>
+    }
+    
+    // Latitude and no error message. 
+    if(!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+
+    // No latitude and no error message.
+    return <div><h1>Loading...</h1></div>
   }
 }
 
